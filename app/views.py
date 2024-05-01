eeac209dc30c8bd245435e654a95757e4a172d97
xavierace990 <xavierparzival@gmail.com>
2024-05-01 06:33:34 -0700

    views.py

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Product
 # Create your views here.
-
+import requests
 
 def product_list(request):
     query = request.GET.get('q')
@@ -14,3 +14,20 @@ def product_list(request):
 def search_list(request):
     
     return render(request, 'search_list.html')
+
+
+def img_upload(image_file):
+    api_key = 'c52a26345162b32e311cf76fea57be22'
+    url = 'https://api.imgbb.com/1/upload'
+
+    with open(image_file, 'rb') as file:
+        payload = {
+            'key': api_key,
+            'image': file,
+        }
+        response = requests.post(url, files=payload)
+
+    if response.status_code == 200:
+        return response.json()['data']['url']
+    else:
+        return None
