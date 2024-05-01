e341bd05cc743fa93601b32acd80b5c525fac559
xavierace990 <xavierparzival@gmail.com>
2024-05-01 06:32:06 -0700

    models.py

--- app/models.py
+++ app/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    #description = models.TextField()
+    # description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='products/')
+    imgbb_url = models.URLField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
